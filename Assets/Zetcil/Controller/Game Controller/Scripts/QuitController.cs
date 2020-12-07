369b7ac7702b4f43e5411315a7a5bd0b49683329
uji <ujiujiuji@student.telkomuniversity.ac.id>
2020-12-07 23:34:19 +0700

    tuga11

---
+++ Assets/Zetcil/Controller/Game Controller/Scripts/QuitController.cs
@@ -0,0 +1,80 @@
+﻿/**************************************************************************************************************
+ * Author : Rickman Roedavan
+ * Version: 2.12
+ * Desc   : Script editor untuk memudahkan pembuatan quit dialog
+ **************************************************************************************************************/
+
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TechnomediaLabs;
+
+namespace Zetcil
+{
+
+    public class QuitController : MonoBehaviour
+    {
+
+        [Space(10)]
+        public bool isEnabled;
+
+        [Header("Invoke Settings")]
+        public GlobalVariable.CInvokeType InvokeType;
+
+        [Header("Delay Settings")]
+        public bool usingDelay;
+        public float Delay;
+
+        [Header("Interval Settings")]
+        public bool usingInterval;
+        public float Interval;
+        
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (isEnabled)
+            {
+                if (InvokeType == GlobalVariable.CInvokeType.OnAwake)
+                {
+                    InvokeQuitController();
+                }
+                if (InvokeType == GlobalVariable.CInvokeType.OnDelay)
+                {
+                    if (usingDelay)
+                    {
+                        Invoke("InvokeQuitController", Delay);
+                    }
+                }
+                if (InvokeType == GlobalVariable.CInvokeType.OnInterval)
+                {
+                    if (usingInterval)
+                    {
+                        InvokeRepeating("InvokeQuitController", 1, Interval);
+                    }
+                }
+            }
+        }
+
+        public void ExecuteQuitController()
+        {
+            InvokeQuitController();
+        }
+
+        public void InvokeQuitController()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+            Debug.Log("Application Quit Execute");
+        }
+    }
+}
---
+++ Assets/Zetcil/Controller/Game Controller/Scripts/TransferController.cs
@@ -0,0 +1,89 @@
+﻿/**************************************************************************************************************
+ * Author : Rickman Roedavan
+ * Version: 2.12
+ * Desc   : Script untuk membuat global transfer antara global variabel
+ **************************************************************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Zetcil
+{
+    public class TransferController : MonoBehaviour
+    {
+        [Space(10)]
+        public bool isEnabled;
+
+        [Header("Global Variable")]
+        public GlobalVariable.CVariableType VariableType;
+        public VarTime TimeSender;
+        public VarTime TimeReceiver;
+        public VarHealth HealthSender;
+        public VarHealth HealthReceiver;
+        public VarMana ManaSender;
+        public VarMana ManaReceiver;
+        public VarExp ExpSender;
+        public VarExp ExpReceiver;
+        public VarScore ScoreSender;
+        public VarScore ScoreReceiver;
+        public VarInteger IntSender;
+        public VarInteger IntReceiver;
+        public VarFloat FloatSender;
+        public VarFloat FloatReceiver;
+        public VarString StringSender;
+        public VarString StringReceiver;
+        public VarBoolean BoolSender;
+        public VarBoolean BoolReceiver;
+
+        [Header("Increment Settings")]
+        public float Increment;
+
+        [System.Serializable]
+        public class CAttributeSettings{
+            public float MinValue;
+            public UnityEvent AttributeEvent;
+        }
+
+        [Header("Attribue Settings")]
+        public bool usingCAttributeSettings;
+        public CAttributeSettings[] AttributeSettings;
+
+        // Use this for initialization
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (isEnabled)
+            {
+                if (VariableType == GlobalVariable.CVariableType.scoreVar)
+                {
+                    if (ScoreReceiver.CurrentValue < ScoreSender.CurrentValue)
+                    {
+                        ScoreReceiver.AddToCurrentValue(Increment + 1);
+                    }
+                    if (ScoreReceiver.CurrentValue >= ScoreSender.CurrentValue)
+                    {
+                        ScoreReceiver.CurrentValue = ScoreSender.CurrentValue;
+
+                        if (usingCAttributeSettings)
+                        {
+                            for (int i = 0; i < AttributeSettings.Length; i++)
+                            {
+                                if (ScoreReceiver.CurrentValue >= AttributeSettings[i].MinValue) {
+                                    AttributeSettings[i].AttributeEvent.Invoke();
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
+    }
+}
